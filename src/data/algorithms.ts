1ada27c4b4f4591300220352a424c6fbf71b59dd
aksh.sh <akshk845@gmail.com>
2026-05-05 11:15:11 +0530

    Update algorithms.ts

--- src/data/algorithms.ts
+++ src/data/algorithms.ts
@@ -140,6 +140,45 @@ end
 
 InitialCost = sum(sum(ICost .* X));
 fprintf('Initial BFS Cost is = %d \\n', InitialCost);`
+  },
+
+  {
+    id: 4,
+    name: "Updated Steepest",
+    code: `format short
+clear all
+clc
+
+syms x1 x2
+f1 = x1-x2+2*x1^2+2*x1*x2+x2^2;
+fx = inline(f1);
+fobj = @(x) fx(x(:,1),x(:,2));
+
+grada = gradient(f1);
+G = inline(grada);
+gradax = @(x) G(x(:,1),x(:,2));
+
+H1 = hessian(f1);
+Hx = inline(H1);
+
+x0 = [1 1];
+maxiter = 4;
+tol = 10^(-3);
+iter = 0;
+X = [];
+
+while norm(gradax(x0)) > tol && iter < maxiter
+    X = [X;x0];
+    S = -gradax(x0);
+    H = Hx(x0);
+    lambda = S'*S ./(S'*H*S);
+    Xnew = x0 + lambda.*S';
+    x0 = Xnew;
+    iter = iter+1;
+end
+
+fprintf('Optimal Solution X=[%f, %f]\\n',x0(1),x0(2))
+fprintf('Optimal Value f(x) = %f \\n',fobj(x0))`
   }
 
 ];
